a5246d71a0066836961931f56a5fc328b6f00c64
matsuriku <192067147+matsuriku420@users.noreply.github.com>
2024-12-18 17:40:02 +0900

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
+import { PluginAPI } from "tailwindcss/types/config";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        text: {
+          main: "var(--color-text-main)",
+          sub: "var(--color-text-sub)",
+        },
+      },
+    },
+  },
+  plugins: [
+    function ({ addBase }: PluginAPI) {
+      addBase({
+        ":root": {
+          "--color-primary": "#2562FF",
+          "--color-secondary": colors.white,
+          "--color-text-main": colors.white,
+          "--color-text-sub": "#00B2FF",
+        },
+      });
+    },
+  ],
+} satisfies Config;
