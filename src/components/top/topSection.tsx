dee31bec1fa9df30d1118a5f5406f6f7940383ac
matsuriku <192067147+matsuriku420@users.noreply.github.com>
2024-12-19 17:26:28 +0900

    top/タイトル文のフォントサイズ調整

--- src/components/top/topSection.tsx
+++ src/components/top/topSection.tsx
@@ -5,15 +5,15 @@ import { TopLinkButton } from "./parts/topLinkButton";
 export const TopSection: React.FC = () => {
   return (
     <div className="bg-primary h-fit w-full">
-      <div className="flex flex-col max-w-fit mx-auto px-2 lg:ml-[185px] pt-[232px]">
+      <div className="flex flex-col max-w-fit mx-auto px-2 lg:ml-[120px] pt-[232px]">
         <p
-          className={`${limeLight.className} text-text-main text-7xl md:text-8xl mr-[120px] leading-[70px] md:leading-[112px] whitespace-pre-wrap`}
+          className={`${limeLight.className} text-text-main text-[12vw] md:text-8xl mr-[120px] leading-[12vw] md:leading-[112px] whitespace-pre-wrap`}
         >
           {`Riku's\nPortfolioSite`}
         </p>
         <div className="h-[1px] bg-text-main w-full mb-[5px]" />
         <p
-          className={`${inter.className} text-text-main font-medium text-2xl md:text-[32px] leading-[37px]`}
+          className={`${inter.className} text-text-main font-medium text-[4vw] md:text-[32px] md:leading-[37px]`}
         >
           松藤利空ポートフォリオサイト
         </p>
