4d5b5c25e1164854ebdf50bb5c94ef5ca797033a
matsuriku420 <192067147+matsuriku420@users.noreply.github.com>
2025-01-04 17:34:02 +0900

    dataCenter/色パレット調整
    ・背景色を他と分離(background)
    ・text-〇〇色を削除、他と統合

--- src/components/top/topSection.tsx
+++ src/components/top/topSection.tsx
@@ -4,16 +4,16 @@ import { TopLinkButton } from "./parts/topLinkButton";
 
 export const TopSection: React.FC = () => {
   return (
-    <div className="bg-primary h-fit w-full">
+    <div className="h-fit w-full">
       <div className="flex flex-col max-w-fit mx-auto px-2 lg:ml-[120px] pt-[232px]">
         <p
-          className={`${limeLight.className} text-text-main text-[12vw] md:text-8xl mr-[120px] leading-[12vw] md:leading-[112px] whitespace-pre-wrap`}
+          className={`${limeLight.className} text-secondary text-[12vw] md:text-8xl mr-[120px] leading-[12vw] md:leading-[112px] whitespace-pre-wrap`}
         >
           {`Riku's\nPortfolioSite`}
         </p>
         <div className="h-[1px] bg-text-main w-full mb-[5px]" />
         <p
-          className={`${inter.className} text-text-main font-medium text-[4vw] md:text-[32px] md:leading-[37px]`}
+          className={`${inter.className} text-secondary font-medium text-[4vw] md:text-[32px] md:leading-[37px]`}
         >
           松藤利空ポートフォリオサイト
         </p>
@@ -21,7 +21,7 @@ export const TopSection: React.FC = () => {
           <TopLinkButton
             label="仮置き(成果物DLページ予定)"
             url="https://twitter.com/"
-            className="bg-black text-text-main"
+            className="bg-black text-secondary"
           />
           <TopLinkButton
             label="仮置き(シアターページ予定)"
