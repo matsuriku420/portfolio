7edbced19c51ba7b63ade1471d54552e4ca109f0
matsuriku420 <192067147+matsuriku420@users.noreply.github.com>
2025-01-04 16:34:05 +0900

    works/カルーセルにインジケーター追加

--- src/components/works/parts/worksProductCardCarousel.tsx
+++ src/components/works/parts/worksProductCardCarousel.tsx
@@ -3,6 +3,7 @@
 import { WorksProps } from "@/datas/worksDatas";
 import React, { useState } from "react";
 import { ProductCard } from "./worksProductCard";
+import { RImage, triangleProps } from "@/datas/imageComponents";
 
 type props = {
   works: WorksProps[];
@@ -11,8 +12,10 @@ type props = {
 
 export const ProductCardCarousel: React.FC<props> = (props) => {
   const [page, SetPageCount] = useState(0);
-  const buttonClassName = "size-20 bg-secondary";
+  const transitionDuration = 300;
+  const buttonClassName = "flex size-14 lg:size-fit transition-all";
   const buttonDisableClassName = "opacity-50 pointer-events-none";
+  const buttonImageScale = 0.5;
 
   return (
     <div
@@ -22,10 +25,11 @@ export const ProductCardCarousel: React.FC<props> = (props) => {
         {props.works.map((work, index) => (
           <div
             key={index}
-            className={`size-full transition-all duration-300 ${
+            className={`size-full transition-all ${
               index != page ? "scale-75 pointer-events-none opacity-70" : ""
             }`}
             style={{
+              transitionDuration: `${transitionDuration}ms`,
               translate: `${100 * (index - page)}%`,
               gridColumnStart: 1,
               gridRowStart: 1,
@@ -35,20 +39,40 @@ export const ProductCardCarousel: React.FC<props> = (props) => {
           </div>
         ))}
       </div>
-      <div className="flex flex-row gap-8">
+      <div className="flex flex-row items-center gap-12">
         <button
           onClick={() => SetPageCount((old) => old - 1)}
-          className={page <= 0 ? buttonDisableClassName : ""}
+          className={`${buttonClassName} ${
+            page <= 0 ? buttonDisableClassName : ""
+          }`}
+          style={{ transitionDuration: `${transitionDuration}ms` }}
         >
-          <div className={buttonClassName} />
+          <RImage
+            data={triangleProps}
+            scale={buttonImageScale}
+            className="rotate-180"
+          />
         </button>
+        <div className="flex flex-row gap-8 items-center h-fit">
+          {props.works.map((value, index) => (
+            <button
+              key={index}
+              onClick={() => SetPageCount(index)}
+              className={`rounded-full bg-secondary transition-all ${
+                index === page ? "size-8" : "size-5"
+              }`}
+              style={{ transitionDuration: `${transitionDuration}ms` }}
+            />
+          ))}
+        </div>
         <button
           onClick={() => SetPageCount((old) => old + 1)}
-          className={
+          className={`${buttonClassName} ${
             page >= props.works.length - 1 ? buttonDisableClassName : ""
-          }
+          }`}
+          style={{ transitionDuration: `${transitionDuration}ms` }}
         >
-          <div className={buttonClassName} />
+          <RImage data={triangleProps} scale={buttonImageScale} />
         </button>
       </div>
     </div>
